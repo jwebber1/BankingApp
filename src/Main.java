f47fecb78d46f9408fb02bc09a5889a74dfd3fe7
hunter-laptop <hberten@missouriwestern.edu>
2018-10-15 19:24:15 -0500

    Test branch commit.

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Banking App");
         primaryStage.setScene(new Scene(root, 640, 480));
         primaryStage.show();
