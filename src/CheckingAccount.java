879e1668ee512af63b6482702b9c6633220bfec5
Jon <34067696+jwebber1@users.noreply.github.com>
2018-10-22 15:45:06 -0500

    Getting rid of HEAD and junk

--- src/CheckingAccount.java
+++ src/CheckingAccount.java
@@ -6,44 +6,16 @@ class CheckingAccount extends Account{
 
     //constructor for the Checking Account
     public CheckingAccount(int cusIDIn, double accBalIn, String accTypeIn, Byte OvProIn, int odThisMonth, Date dateAccOpened){
-<<<<<<< HEAD
-<<<<<<< HEAD
-        this.customerID = cusIDIn;
-        this.accountBalance = accBalIn;
-=======
-        super(cusIDIn,accBalIn);
->>>>>>> JonTextInBranch
-        this.accountType = accTypeIn;
-=======
+
         super(cusIDIn,accBalIn, dateAccOpened, accTypeIn);
->>>>>>> JonTextInBranch
         this.indicatedOverdraftProtection = OvProIn;
         this.overdraftsThisMonth = odThisMonth;
     }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    //getters
-    public String getAccountType() {return accountType;}
-    public Byte getIndicatedOverdraftProtection() {return indicatedOverdraftProtection;}
-    public int getOverdraftsThisMonth() {return overdraftsThisMonth;}
-    public Date getDateAccountOpened() {return dateAccountOpened;}
-
-    //setters
-    public void setAccountType(String accountType) {this.accountType = accountType;}
-    public void setIndicatedOverdraftProtection(Byte indicatedOverdraftProtection) {this.indicatedOverdraftProtection = indicatedOverdraftProtection;}
-    public void setOverdraftsThisMonth(int overdraftsThisMonth) {this.overdraftsThisMonth = overdraftsThisMonth;}
-    public void setDateAccountOpened(Date dateAccountOpened) {this.dateAccountOpened = dateAccountOpened;}
-
-=======
->>>>>>> JonTextInBranch
-    //other methods to be decided
-=======
     public Byte getIndicatedOverdraftProtection() {return indicatedOverdraftProtection;}
     public void setIndicatedOverdraftProtection(Byte indicatedOverdraftProtection) {this.indicatedOverdraftProtection = indicatedOverdraftProtection;}
     public int getOverdraftsThisMonth() {return overdraftsThisMonth;}
     public void setOverdraftsThisMonth(int overdraftsThisMonth) {this.overdraftsThisMonth = overdraftsThisMonth;}
->>>>>>> JonTextInBranch
 
     @Override
     public String toString() {
