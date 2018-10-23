3bf9401578b9cf92d9d1f303d84d32f0d91c6ef3
Jon <34067696+jwebber1@users.noreply.github.com>
2018-10-22 21:01:56 -0500

    Update Customer.java

--- src/Customer.java
+++ src/Customer.java
@@ -1,25 +1,19 @@
-public class Customer {
-    private int socialSecurityNumber;    //customerID in account classes
+public class Customer extends Person {
     private String streetAddress;
     private String city;
     private String state;
     private String zipCode;
-    private String firstName;
-    private String lastName;
+
 
     //constructor for the Customer class
-    public Customer (int ssn, String addr, String city, String state, String zip, String fName, String lName){
-        this.socialSecurityNumber = ssn;
+    public Customer (int ssn, String addr, String city, String state, String zip, String fName, String lName, String pin){
+        super(ssn, fName, lName, pin);
         this.streetAddress = addr;
         this.city = city;
         this.state = state;
         this.zipCode = zip;
-        this.firstName = fName;
-        this.lastName = lName;
     }
 
-    public int getSocialSecurityNumber() {return socialSecurityNumber;}
-    public void setSocialSecurityNumber(int socialSecurityNumber) {this.socialSecurityNumber = socialSecurityNumber;}
     public String getStreetAddress() {return streetAddress;}
     public void setStreetAddress(String streetAddress) {this.streetAddress = streetAddress;}
     public String getCity() {return city;}
@@ -28,25 +22,22 @@ public class Customer {
     public void setState(String state) {this.state = state;}
     public String getZipCode() {return zipCode;}
     public void setZipCode(String zipCode) {this.zipCode = zipCode;}
-    public String getFirstName() {return firstName;}
-    public void setFirstName(String firstName) {this.firstName = firstName;}
-    public String getLastName() {return lastName;}
-    public void setLastName(String lastName) {this.lastName = lastName;}
 
     @Override
     public String toString() {
         return "Customer{" +
-                "socialSecurityNumber=" + socialSecurityNumber +
-                ", streetAddress='" + streetAddress + '\'' +
+                "streetAddress='" + streetAddress + '\'' +
                 ", city='" + city + '\'' +
                 ", state='" + state + '\'' +
                 ", zipCode='" + zipCode + '\'' +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                ", fName='" + fName + '\'' +
+                ", lName='" + lName + '\'' +
+                ", password='" + password + '\'' +
                 '}';
     }
 
     //other methods to be decided
 
 
-}
+}//end of Customer
