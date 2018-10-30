cbd822a7e00b8af05a301be553df9fb1f2e5e430
JacobTea <jteaford1@missouriwestern.edu>
2018-10-30 10:15:46 -0500

    Merge fix

--- src/SavingAccount.java
+++ src/SavingAccount.java
@@ -3,7 +3,7 @@ import java.util.Date;
 class SavingAccount extends Account {
     protected double currentInterestRate;
     protected Date dateCDDue;
-
+    //fix for merge
 
     //constructor for the SavingAccount
     public SavingAccount(int cusIDIn, double accBalIn, double currIntRateIn, Date dateAccOpenedIn) {
