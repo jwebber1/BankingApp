c11f248065977a4d2002af7d3f4386f2374d3a0a
JacobTea <jteaford1@missouriwestern.edu>
2018-10-30 10:06:42 -0500

    Fix for access

--- src/SavingAccount.java
+++ src/SavingAccount.java
@@ -1,19 +1,19 @@
 import java.util.Date;
 
-class SavingAccount extends Account{
+class SavingAccount extends Account {
     protected double currentInterestRate;
     protected Date dateCDDue;
-    //Jacob Testing his git connection
+
 
     //constructor for the SavingAccount
-    public SavingAccount(int cusIDIn, double accBalIn, double currIntRateIn, Date dateAccOpenedIn){
+    public SavingAccount(int cusIDIn, double accBalIn, double currIntRateIn, Date dateAccOpenedIn) {
         super(cusIDIn, accBalIn, dateAccOpenedIn, "Saving");
         this.currentInterestRate = currIntRateIn;
         this.dateCDDue = null;
     }
 
     //overloading SavingAccount to CD
-    public SavingAccount(int cusIDIn, double accBalIn, double currIntRateIn, Date dateAccOpenedIn, Date dateCDDueIn){
+    public SavingAccount(int cusIDIn, double accBalIn, double currIntRateIn, Date dateAccOpenedIn, Date dateCDDueIn) {
         super(cusIDIn, accBalIn, dateAccOpenedIn, "CD");
         this.customerID = cusIDIn;
         this.accountBalance = accBalIn;
@@ -21,19 +21,30 @@ class SavingAccount extends Account{
         this.dateCDDue = dateCDDueIn;
     }
 
-    public double getCurrentInterestRate() {return currentInterestRate;}
-    public void setCurrentInterestRate(double currentInterestRate) {this.currentInterestRate = currentInterestRate;}
-    public Date getDateCDDue() {return dateCDDue;}
-    public void setDateCDDue(Date dateCDDue) {this.dateCDDue = dateCDDue;}
+    public double getCurrentInterestRate() {
+        return currentInterestRate;
+    }
+
+    public void setCurrentInterestRate(double currentInterestRate) {
+        this.currentInterestRate = currentInterestRate;
+    }
+
+    public Date getDateCDDue() {
+        return dateCDDue;
+    }
+
+    public void setDateCDDue(Date dateCDDue) {
+        this.dateCDDue = dateCDDue;
+    }
 
     @Override
     public String toString() {
-        return "SavingAccount{" + 
-                "currentInterestRate=" + currentInterestRate + 
-                ", dateAccountOpened=" + dateAccountOpened + 
-                ", dateCDDue=" + dateCDDue + 
-                ", customerID=" + customerID + 
-                ", accountBalance=" + accountBalance + 
+        return "SavingAccount{" +
+                "currentInterestRate=" + currentInterestRate +
+                ", dateAccountOpened=" + dateAccountOpened +
+                ", dateCDDue=" + dateCDDue +
+                ", customerID=" + customerID +
+                ", accountBalance=" + accountBalance +
                 '}';
     }
 
