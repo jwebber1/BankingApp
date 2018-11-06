7220c43630c9a1bdee10f97b8603ddd2fb749ee9
Jon <34067696+jwebber1@users.noreply.github.com>
2018-11-06 08:06:24 -0600

    Got rid of excess in main

--- src/Main.java
+++ src/Main.java
@@ -12,25 +12,6 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class Main extends Application {
-    //used to format money, will probably need to be moved to "controller" for the GUI
-    static NumberFormat moneyFormat = NumberFormat.getCurrencyInstance();
-
-    static ArrayList<Person> people;
-    static ArrayList<CheckingAccount> checkingAccounts;
-    static ArrayList<Check> checks;
-    static ArrayList<CD> cds;
-
-    static {
-        try {
-            people = Person.importFile();
-            checkingAccounts = CheckingAccount.importFile();
-            checks = Check.importFile();
-            cds = CD.importFile();
-        }
-        catch (IOException e) {e.printStackTrace();}
-        catch (ParseException e) {e.printStackTrace();}
-    }
-
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -42,19 +23,6 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
-
-        Person.debugImport();
-        CheckingAccount.debugImport();
-        Check.debugImport();
-
-
-        try {
-            Person.exportFile(people);
-            CheckingAccount.exportFile(checkingAccounts);
-            Check.exportFile(checks);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
 }//end of main
