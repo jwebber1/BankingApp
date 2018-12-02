5c2c0a5108c07534cb74ef1ada721b662ded404f
jteaford1 <jteaford1@missouriwestern.edu>
2018-12-02 15:12:26 -0600

    Cleaning and Commenting

--- src/CD.java
+++ src/CD.java
@@ -17,16 +17,13 @@ public class CD extends Account {
 
     CD(int cusIDIn, double accBalIn, double currIntRateIn, Date dateAccOpenedIn, Date dateCDDueIn, int cdNumber) {
         super(cusIDIn, accBalIn, dateAccOpenedIn, "CD");
-
         mainAccountType = "CD";
-
         this.customerID = cusIDIn;
         this.accountBalance = accBalIn;
         this.currentInterestRate = currIntRateIn;
         this.dateCDDue = dateCDDueIn;
         this.cdNumber = cdNumber;
     }
-
 
     //Create import, export, and search by cusID
     public double getCurrentInterestRate() { return currentInterestRate; }
@@ -58,7 +55,6 @@ public class CD extends Account {
 
                 //split the line into an array of strings
                 String[] splitLine = line.split(",");
-
                 //create temp variable to hold info from the split lines
                 int cusID = Integer.parseInt(splitLine[0]);
                 double balance = Double.parseDouble(splitLine[1]);
@@ -66,14 +62,12 @@ public class CD extends Account {
                 Date dateAccountOpened = new SimpleDateFormat("MM/dd/yyyy").parse(splitLine[3]);
                 Date dateCdDue = new SimpleDateFormat("MM/dd/yyyy").parse(splitLine[4]);
                 int cdNum = Integer.parseInt(splitLine[5]);
-
                 //add the new data to the ArrayList
                 cds.add(new CD(cusID, balance, currentInterest, dateAccountOpened, dateCdDue, cdNum));
 
                 //Debugging
                 //System.out.println("count: " + (lineNum) + "\t" + cds.get(lineNum - 1).toString());
             }
-
             //increment the line number
             lineNum++;
         }
@@ -88,10 +82,8 @@ public class CD extends Account {
         //create a new PrintWriter to write to a file
         PrintWriter cdWriter = new PrintWriter(new FileOutputStream("memory/cds.txt", false));
         SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
-
         //printing the headers of the files
         cdWriter.println("CustomerID,AccountBalance,CurrInterestRate,DateAccOpened,DateCDDue,CDNumber");
-
         for (CD cd : cds) {
 
             //format the line to put back into the file
@@ -101,18 +93,14 @@ public class CD extends Account {
                     formatter.format(cd.getDateAccountOpened()) + "," +
                     formatter.format(cd.getDateCDDue()) + "," +
                     cd.getCdNumber() + ",");
-
             cdWriter.flush();
         }
-
         //close the PrintWriter objects
         cdWriter.flush();
         cdWriter.close();
-
     }//end of exportFile()
 
     static ArrayList<CD> search(int custID) {
-
         //search by cusID, shows all CD
         ArrayList<CD> searchResults = new ArrayList<>();
         for (CD cd : cds) {
@@ -120,7 +108,6 @@ public class CD extends Account {
                 searchResults.add(cd);
             }
         }
-
         //return found checking accounts OR null
         return searchResults;
     }
@@ -149,8 +136,6 @@ public class CD extends Account {
             //This only gives the original balance as because it is before the due date
             amtToReturn = getAccountBalance();
         }
-
         return amtToReturn;
     }//end of CD withdraw
-
 }//end of CD
