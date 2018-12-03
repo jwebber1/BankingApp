096150e19616a725709e27a707af0b8eb51ee762
Jon <34067696+jwebber1@users.noreply.github.com>
2018-12-02 20:21:55 -0600

    Clean Code

--- src/Account.java
+++ src/Account.java
@@ -6,10 +6,9 @@ public class Account {
     protected Date dateAccountOpened;
     protected String accountType;
     protected String mainAccountType; // I need this to display savings/checking/loans for the UI
-    //SAVINGS:   Saving = "S"   CD = "CD"
-    //CHECKINGS: "regular" or "gold"
-    //LOANS:     long term = "LT"  short term = "ST"   credit card = "CC"
+    //SAVINGS:   Saving = "S"   CD = "CD"   CHECKINGS: "regular" or "gold"  LOANS:  long term = "LT"  short term = "ST"   credit card = "CC"
 
+    //constructor
     public Account(int cusIDIn, double accBalIn, Date dateAccOpIn, String accTypeIn) {
         this.customerID = cusIDIn;
         this.accountBalance = accBalIn;
@@ -19,16 +18,9 @@ public class Account {
 
     //getters/setters
     public int getCustomerID() {return customerID;}
-    public void setCustomerID(int customerID) {this.customerID = customerID;}
     public double getAccountBalance() {return accountBalance;}
     public void setAccountBalance(double accountBalance) {this.accountBalance = accountBalance;}
     public Date getDateAccountOpened() {return dateAccountOpened;}
-    public void setDateAccountOpened(Date dateAccountOpened) {this.dateAccountOpened = dateAccountOpened;}
     public String getAccountType() {return accountType;}
     public void setAccountType(String accountType) {this.accountType = accountType;}
-    public String getMainAccountType() {return mainAccountType;}
-    public void setMainAccountType(String mainAccountType) {this.mainAccountType = mainAccountType;}
-
-    //other methods to be decided
-
 }//end of Account
